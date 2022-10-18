1b7de1308c67028a17768e5be4c143eb37fb861d
ShaunA-R <shaunreyes209@gmail.com>
2022-10-18 11:41:45 -0700

    First Push

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿//Shaun Aguirre-Reyes
+// 10/18/22
+// This program the computer decides whether a number is odd or even
+
+
+
+Console.Clear();
+
+int i;
+string playAgain = "yes";
+while(playAgain == "yes"){
+
+
+Console.Write("Enter a Number: ");
+i = int.Parse(Console.ReadLine());
+if (i % 2 == 0)
+{
+Console.Write("Entered Number is an Even Number. ");
+}else
+{
+Console.Write("Entered Number is an Odd Number. ");
+}
+Console.WriteLine("Would you like to play again?");
+
+playAgain = Console.ReadLine();
+
+if(playAgain == "yes"){
+    Console.WriteLine("Lets try again");
+}else{
+    Console.WriteLine("Have an amazing day then :)");
+}
+}
